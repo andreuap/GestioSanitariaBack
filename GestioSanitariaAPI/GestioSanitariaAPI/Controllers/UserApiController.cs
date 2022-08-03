3bd181946488bfdb08531cf7202cfd3bf97fac2e
Andreu Aragay <aaragaypagan@gmail.com>
2022-08-03 17:26:18 +0200

    Modified User Controller to create an endpoint

--- GestioSanitariaAPI/GestioSanitariaAPI/Controllers/UserApiController.cs
+++ GestioSanitariaAPI/GestioSanitariaAPI/Controllers/UserApiController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestioSanitariaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestioSanitariaAPI.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
     public class UserApiController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User { Id = new Guid(), UserName = "andreu", Password = "123456", Email = "andreu@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false },
+                new User { Id = new Guid(), UserName = "anna", Password = "7895", Email = "anna@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false }
+            };
+        }
     }
 }
