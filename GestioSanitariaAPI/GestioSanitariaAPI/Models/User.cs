819fe2c70eb43d192d51d7283b37d2820e881599
Andreu Aragay <aaragaypagan@gmail.com>
2022-08-03 17:01:36 +0200

    Create model user class

---
+++ GestioSanitariaAPI/GestioSanitariaAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace GestioSanitariaAPI.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DataAlta { get; set; }
+        public DateTime? DataBaixa { get; set; }
+        public bool EsBloquejat { get; set; }
+    }
+}
