6e26e1d05e77d94047457368f721389a8a956f7a
Andreu Aragay <aaragaypagan@gmail.com>
2022-08-05 08:57:38 +0200

    Add Model State validations

--- GestioSanitariaAPI/GestioSanitariaAPI/Controllers/UserApiController.cs
+++ GestioSanitariaAPI/GestioSanitariaAPI/Controllers/UserApiController.cs
@@ -8,19 +8,21 @@ namespace GestioSanitariaAPI.Controllers
     [ApiController]
     public class UserApiController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<UserDTO>> GetUsers()
-        {
-            return Ok(new List<UserDTO>
+        List<UserDTO> userList = new List<UserDTO>
             {
                 new UserDTO { Id = new Guid("f6f020cc-edb7-4809-bf60-8f42b3e67932"), UserName = "andreu", Password = "123456", Email = "andreu@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false },
                 new UserDTO { Id = new Guid("523e1b90-fad9-41be-9f63-0848d2471b30"), UserName = "anna", Password = "7895", Email = "anna@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false }
-            });
+            };
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<UserDTO>> GetUsers()
+        {
+            return Ok(userList);
         }
         [HttpGet("{id:guid}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<UserDTO> GetUser(Guid Id)
         {
             if (Id == Guid.Empty)
@@ -28,11 +30,7 @@ namespace GestioSanitariaAPI.Controllers
                 return BadRequest();
             }
 
-            var user = new List<UserDTO>
-            {
-                new UserDTO { Id = new Guid("f6f020cc-edb7-4809-bf60-8f42b3e67932"), UserName = "andreu", Password = "123456", Email = "andreu@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false },
-                new UserDTO { Id = new Guid("523e1b90-fad9-41be-9f63-0848d2471b30"), UserName = "anna", Password = "7895", Email = "anna@example.com", DataAlta = new DateTime(), DataBaixa = null, EsBloquejat = false }
-            };
+            var user = userList.Select(x => x.Id = Id);
 
             if (user == null)
             {
@@ -45,16 +43,17 @@ namespace GestioSanitariaAPI.Controllers
         [HttpPost]
         public ActionResult<UserDTO> CreateUser([FromBody]UserDTO userDTO)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             if(userDTO == null)
             {
                 return BadRequest(userDTO);
             }
-            if(userDTO.Id !=Guid.Empty)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-
+            
             userDTO.Id = new Guid();
+            userList.AddRange(userDTO);
             return Ok(userDTO);
         }
     }
--- GestioSanitariaAPI/GestioSanitariaAPI/Models/Dto/UserDTO.cs
+++ GestioSanitariaAPI/GestioSanitariaAPI/Models/Dto/UserDTO.cs
@@ -1,13 +1,23 @@
-﻿namespace GestioSanitariaAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestioSanitariaAPI.Models.Dto
 {
     public class UserDTO
     {
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string UserName { get; set; }
+        [Required]
+        [MinLength(8)]
         public string Password { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Email { get; set; }
+        [Required]
         public DateTime DataAlta { get; set; }
         public DateTime? DataBaixa { get; set; }
+        [Required]
         public bool EsBloquejat { get; set; }
     }
 }
